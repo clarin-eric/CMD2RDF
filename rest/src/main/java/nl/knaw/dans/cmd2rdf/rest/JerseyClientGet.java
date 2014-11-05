5ead77818253d61cdacb46d1109cb5b9a3d349a7
Eko Indarto on Mac <eko.indarto@dans.knaw.nl>
2014-11-05 16:16:37 +0100

    Intermediate commit

--- rest/src/main/java/nl/knaw/dans/cmd2rdf/rest/JerseyClientGet.java
+++ rest/src/main/java/nl/knaw/dans/cmd2rdf/rest/JerseyClientGet.java
@@ -1,48 +1,55 @@
 package nl.knaw.dans.cmd2rdf.rest;
 
+import java.io.InputStream;
 import java.net.URI;
-import java.net.URLEncoder;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 
+import org.apache.commons.io.IOUtils;
 import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.uri.UriComponent;
+import org.glassfish.jersey.uri.UriComponent.Type;
 
 public class JerseyClientGet {
 	public static void main(String[] args) {
 		try {
-	 
-			Client client = ClientBuilder.newClient().register(JacksonFeature.class);
-	 
-			UriBuilder uriBuilder = UriBuilder.fromUri(new URI("http://zandbak01.dans.knaw.nl:8000"));
+
+			Client client = ClientBuilder.newClient().register(
+					JacksonFeature.class);
+			UriBuilder uriBuilder = UriBuilder.fromUri(new URI(
+					"http://localhost:8890"));
 			uriBuilder.path("sparql");
-			uriBuilder.queryParam("query", "select distinct %3FConcept where {[] a %3FConcept} LIMIT 100");
-			uriBuilder.queryParam("format", "application/sparql-results+json");
-			WebTarget target = client.target(uriBuilder.build());
-			
+			uriBuilder.queryParam("query", UriComponent.encode(
+					"select distinct ?Concept where {[] a ?Concept} LIMIT 100",
+					Type.QUERY));
+			uriBuilder.queryParam("format", "applicationjson");
+			WebTarget target = client.target(uriBuilder);
+
 			Response response = target.request().get();
+
 			int status = response.getStatus();
-	 
+
 			if (response.getStatus() != 200) {
-			   throw new RuntimeException("Failed : HTTP error code : "
-				+ response.getStatus());
+				throw new RuntimeException("Failed : HTTP error code : "
+						+ response.getStatus());
+
+			} else {
+				InputStream in = response.readEntity(InputStream.class);
+				String theString = IOUtils.toString(in, "UTF-8");
+				System.out.println(theString);
 			}
-	 
-			String output = response.getEntity().toString();
-	 
+
 			System.out.println("Output from Server .... \n");
-			System.out.println(output);
-	 
-		  } catch (Exception e) {
-	 
+
+		} catch (Exception e) {
+
 			e.printStackTrace();
-	 
-		  }
-	 
+
 		}
+
+	}
 }
--- rest/src/main/java/nl/knaw/dans/cmd2rdf/rest/Query.java
+++ rest/src/main/java/nl/knaw/dans/cmd2rdf/rest/Query.java
@@ -1,38 +1,57 @@
 package nl.knaw.dans.cmd2rdf.rest;
 
+import java.util.Iterator;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
-@Path("/sparql")
+@Path("/")
 public class Query {
 
 	/**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return 'SPARQL Query' (String) that will be returned as a text/plain response.
-     */
-  @GET
-  @Produces(MediaType.TEXT_PLAIN)
-  public String sayPlainTextHello() {
-    return "SPARQL Query!";
-  }
-  
-  @GET
-	@Path("/get")
-	public Response addUser(@Context HttpHeaders headers, @Context UriInfo uriInfo) {
-		  String query = uriInfo.getRequestUri().getQuery();
+	 * Method handling HTTP GET requests. The returned object will be sent to
+	 * the client as "text/plain" media type.
+	 *
+	 * @return 'SPARQL Query' (String) that will be returned as a text/plain
+	 *         response.
+	 */
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayPlainTextHello() {
+		return "SPARQL Query!";
+	}
+
+	@GET
+	@Path("/sparql")
+	public Response addUser(@Context HttpHeaders headers,
+			@Context UriInfo uriInfo) {
+		String query = uriInfo.getRequestUri().getQuery();
+		MultivaluedMap<String, String> mm = headers.getRequestHeaders();
+		Iterator<String> it = mm.keySet().iterator();
+
+		while (it.hasNext()) {
+			String s = it.next();
+			System.out.println(s);
+			List<String> ls = mm.get(s);
+			for (String ss : ls) {
+				System.out.println("ss: " + ss);
+
+			}
+		}
 		String userAgent = headers.getRequestHeader("user-agent").get(0);
 
-		return Response.status(200)
-			.entity("addUser is called, userAgent : " + userAgent + "\n========\n" + query)
-			.build();
+		return Response
+				.status(200)
+				.entity("addUser is called, userAgent : " + userAgent
+						+ "\n========\n" + query).build();
 
 	}
-} 
+}
