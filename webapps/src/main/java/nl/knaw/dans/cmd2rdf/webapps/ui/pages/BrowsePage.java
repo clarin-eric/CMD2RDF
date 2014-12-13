9c295cb1e5d1151c0c37cf0ed5169c8a746a8fd2
ekoi <eko.indarto@dans.knaw.nl>
2014-12-13 21:58:25 +0100

    Remove hardcoded code

--- webapps/src/main/java/nl/knaw/dans/cmd2rdf/webapps/ui/pages/BrowsePage.java
+++ webapps/src/main/java/nl/knaw/dans/cmd2rdf/webapps/ui/pages/BrowsePage.java
@@ -2,6 +2,12 @@ package nl.knaw.dans.cmd2rdf.webapps.ui.pages;
 
 import nl.knaw.dans.cmd2rdf.webapps.ui.Cmd2RdfBasePage;
 
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.html.link.InlineFrame;
+import org.apache.wicket.markup.html.pages.RedirectPage;
+import org.apache.wicket.protocol.http.RequestUtils;
+import org.apache.wicket.request.Url;
+import org.apache.wicket.request.cycle.RequestCycle;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 /**
@@ -23,8 +29,11 @@ public class BrowsePage extends Cmd2RdfBasePage
 	{
 		super(pageParameters);
 
+		Url relative = Url.parse("graph/browse");
+		String cmd2rdfLdaUrl = getRequestCycle().getUrlRenderer().renderFullUrl(relative).replace(getRequestCycle().getRequest().getContextPath(), "/cmd2rdf-lda");
+		RedirectPage page = new RedirectPage ( cmd2rdfLdaUrl );
+		InlineFrame frame = new InlineFrame("cmd2rdfLdaUrl", page);
+		add(frame);
 		
 	}
-
-	
 }
